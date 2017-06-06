12a0118c975433b3843b842b9a7a9e6b3a38471e
NikolayKuznesov <Dreg-2@yandex.ru>
2017-06-07 00:04:21 +0300

    Add scripts slider cube

--- app/scripts/pageController.js
+++ app/scripts/pageController.js
@@ -1,14 +1,8 @@
-$(function(){
+$(function () {
 
-    $(window).scroll(function(){
-        if( $(window).scrollTop() > 50 ) {
-            $('header').addClass('scrolled');
-            $('header .logo .icon-logo').addClass('black');
-            $('header .icon-social-link').addClass('black');
-        } else {
-            $('header').removeClass('scrolled');
-            $('header .logo .icon-logo').removeClass('black');
-            $('header .icon-social-link').removeClass('black');
-        }
+    $(window).scroll(function () {
+
     });
+
+
 });
