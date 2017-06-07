fc7be440b252e6857cb833d29e3ec43df97a4933
NikolayKuznesov <Dreg-2@yandex.ru>
2017-06-08 00:09:01 +0300

    styles and block 4 animation

--- app/scripts/pageController.js
+++ app/scripts/pageController.js
@@ -20,6 +20,18 @@ $(function () {
     var mousePointerBlockThree7 = $('.block--block-three .img-element.img-element-7');
     var mousePointerBlockThree8 = $('.block--block-three .img-element.img-element-8');
 
+    var mousePointerBlockFour1 = $('.block--block-four .img-element.img-element-1');
+    var mousePointerBlockFour2 = $('.block--block-four .img-element.img-element-2');
+    var mousePointerBlockFour3 = $('.block--block-four .img-element.img-element-3');
+    var mousePointerBlockFour4 = $('.block--block-four .img-element.img-element-4');
+    var mousePointerBlockFour5 = $('.block--block-four .img-element.img-element-5');
+    var mousePointerBlockFour6 = $('.block--block-four .img-element.img-element-6');
+    var mousePointerBlockFour7 = $('.block--block-four .img-element.img-element-7');
+    var mousePointerBlockFour8 = $('.block--block-four .img-element.img-element-8');
+    var mousePointerBlockFour9 = $('.block--block-four .img-element.img-element-9');
+    var mousePointerBlockFour10 = $('.block--block-four .img-element.img-element-10');
+    var mousePointerBlockFour11 = $('.block--block-four .img-element.img-element-11');
+
     $(window).mousemove(function (e) {
 
         var x = e.pageX / window.innerHeight;
@@ -27,10 +39,10 @@ $(function () {
         x = x * -60;
         y = y * -100;
         var transfromPosition1 = 'translate3d(' + x + '%, ' + y + '%, 0)';
-        var transfromPosition2 = 'translate3d(' + (x/2) + '%, 0, 0)';
-        var transfromPosition3 = 'translate3d(' + x + '%, ' + (-y*2) + '%, 0)';
-        var transfromPosition4 = 'translate3d(' + (x-10) + '%, ' + y + '%, 0)';
-        var transfromPosition5 = 'translate3d(' + (x+25) + '%, ' + 0 + '%, 0)';
+        var transfromPosition2 = 'translate3d(' + (x / 2) + '%, 0, 0)';
+        var transfromPosition3 = 'translate3d(' + x + '%, ' + (-y * 2) + '%, 0)';
+        var transfromPosition4 = 'translate3d(' + (x - 10) + '%, ' + y + '%, 0)';
+        var transfromPosition5 = 'translate3d(' + (x + 25) + '%, ' + 0 + '%, 0)';
 
         mousePointerBlockTwo1.css('transform', transfromPosition1);
         mousePointerBlockTwo2.css('transform', transfromPosition2);
@@ -39,10 +51,10 @@ $(function () {
         mousePointerBlockTwo5.css('transform', transfromPosition5);
         mousePointerBlockTwo6.css('transform', transfromPosition4);
 
-        transfromPosition2 = 'translate3d(' + (x/2) + '%, 0, 0)';
-        transfromPosition3 = 'translate3d(' + (x+25) + '%, ' + 0 + '%, 0)';
-        transfromPosition4 = 'translate3d(' + (x-10) + '%, ' + (y+18) + '%, 0)';
-        transfromPosition5 = 'translate3d(' + x/2 + '%, ' + (y*3) + '%, 0)';
+        transfromPosition2 = 'translate3d(' + (x / 2) + '%, 0, 0)';
+        transfromPosition3 = 'translate3d(' + (x + 25) + '%, 0, 0)';
+        transfromPosition4 = 'translate3d(' + (x - 10) + '%, ' + (y + 18) + '%, 0)';
+        transfromPosition5 = 'translate3d(' + x / 2 + '%, ' + (y * 3) + '%, 0)';
 
 
         mousePointerBlockThree1.css('transform', transfromPosition1);
@@ -53,6 +65,19 @@ $(function () {
         mousePointerBlockThree6.css('transform', transfromPosition5);
         mousePointerBlockThree7.css('transform', transfromPosition4);
         mousePointerBlockThree8.css('transform', transfromPosition2);
+
+
+        mousePointerBlockFour1.css('transform', transfromPosition1);
+        mousePointerBlockFour2.css('transform', transfromPosition2);
+        mousePointerBlockFour3.css('transform', transfromPosition3);
+        mousePointerBlockFour4.css('transform', transfromPosition4);
+        mousePointerBlockFour5.css('transform', transfromPosition5);
+        mousePointerBlockFour6.css('transform', transfromPosition2);
+        mousePointerBlockFour7.css('transform', transfromPosition3);
+        mousePointerBlockFour8.css('transform', transfromPosition1);
+        mousePointerBlockFour9.css('transform', transfromPosition3);
+        mousePointerBlockFour10.css('transform', transfromPosition2);
+        mousePointerBlockFour11.css('transform', transfromPosition4);
     })
 
 });
